f973e874604b5d332d9e902b2e0645a31e04a6af
Danniar Reza Firdausy <danniarreza@gmail.com>
2023-01-30 16:34:15 +0100

    Initial commit

---
+++ src/main/java/nl/utwente/clicksproject/otmdataapp/OtmdataappApplication.java
@@ -0,0 +1,13 @@
+package nl.utwente.clicksproject.otmdataapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtmdataappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtmdataappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/utwente/clicksproject/otmdataapp/OtmdataappApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.utwente.clicksproject.otmdataapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtmdataappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
